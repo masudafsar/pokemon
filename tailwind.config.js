c8cfd4060c39ca2eb75d0cb22e2431964847d6f3
Masud Afsar <masud@afsar.info>
2022-05-19 15:14:31 +0430

    Insert poppin font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
       colors: {
         tags: {
           normal: '#929DA3',
