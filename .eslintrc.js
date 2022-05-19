dd0e64abf9f8911519d5bf811fe1ef1aefc13b43
Masud Afsar <masud@afsar.info>
2022-05-19 12:35:27 +0430

    Install react packages and config them
    - axios
    - react router
    - tailwind
    - sass
    - eslint

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'react-hooks',
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  rules: {
+    'max-len': ['warn', { code: 120 }],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx'] }],
+    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': ['error'],
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        allowExpressions: true,
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'import/prefer-default-export': 'off',
+    'react/prop-types': 'off',
+  },
+};
---
+++ src/layouts/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './App.module.scss';
+
+const App: React.FC = () => (
+  <div className={styles.App}>
+    <h1 className="text-blue-500">Hi Pokémon</h1>
+  </div>
+);
+
+export default App;
