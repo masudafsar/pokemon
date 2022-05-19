9444bbbfbceec6df307849da4f6de6b0cb4de901
Masud Afsar <masud@afsar.info>
2022-05-19 12:49:18 +0430

    Implement get request hook

---
+++ src/hooks/useGetRequest.ts
@@ -0,0 +1,32 @@
+import { DependencyList, useEffect, useState } from 'react';
+import request from '../helpers/request';
+import { useRequestType } from '../types/api/useRequestType';
+
+export const useGetRequest = <ResponseT, ParamsT = any>(
+  url: string,
+  params?: ParamsT,
+  deps: DependencyList = [],
+): useRequestType<ResponseT> => {
+  const [data, setData] = useState<ResponseT>();
+  const [error, setError] = useState(undefined);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      setIsLoaded(false);
+      try {
+        const { data: response } = await request.get<ResponseT>(url, {
+          params,
+        });
+        setData(response);
+        setIsLoaded(true);
+      } catch (e: any) {
+        setError(e);
+      }
+    };
+
+    fetchData();
+  }, deps);
+
+  return [data, isLoaded, error];
+};
---
+++ src/types/api/useRequestType.ts
@@ -0,0 +1,1 @@
+export type useRequestType<T> = [T | undefined, boolean, any];
